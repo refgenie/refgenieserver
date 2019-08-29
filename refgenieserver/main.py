12f577bd947136fc1f1519cf7ed0542cdc8f9da3
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2019-08-29 13:32:06 -0400

    do not set the routers up for archiving

--- refgenieserver/main.py
+++ refgenieserver/main.py
@@ -20,24 +20,24 @@ def main():
     args = parser.parse_args()
     logger_args = dict(name=PKG_NAME, fmt=LOG_FORMAT, level=5) if args.debug else dict(name=PKG_NAME, fmt=LOG_FORMAT)
     _LOGGER = logmuse.setup_logger(**logger_args)
-    rgc = RefGenConf(select_genome_config(args.config))
-    # the router imports need to be after the RefGenConf object is declared
-    from .routers import version1, version2
-    app.include_router(version1.router)
-    app.include_router(
-        version1.router,
-        prefix="/v1"
-    )
-    app.include_router(
-        version2.router,
-        prefix="/v2"
-    )
+    selected_cfg = select_genome_config(args.config)
+    rgc = RefGenConf(selected_cfg)
     assert len(rgc) > 0, "You must provide a config file or set the '{}' environment variable".\
         format(", ".join(CFG_ENV_VARS))
     if args.command == "archive":
-        archive(rgc, args)
+        archive(rgc, args.genome, args.asset, args.force, selected_cfg)
     elif args.command == "serve":
-        _LOGGER.info("serving")
+        # the router imports need to be after the RefGenConf object is declared
+        from .routers import version1, version2
+        app.include_router(version1.router)
+        app.include_router(
+            version1.router,
+            prefix="/v1"
+        )
+        app.include_router(
+            version2.router,
+            prefix="/v2"
+        )
         uvicorn.run(app, host="0.0.0.0", port=args.port)
 
 
