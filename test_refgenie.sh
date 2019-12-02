710d96078c8a683feb28b349034797b8b51abc70
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2019-12-02 08:46:40 -0500

    updates after yacman release

--- test_refgenie.sh
+++ test_refgenie.sh
@@ -61,7 +61,6 @@ LABEL authors="Nathan Sheffield, Michal Stolarczyk"
 
 COPY . /app
 
-RUN pip install https://github.com/databio/yacman/archive/dev.zip
 RUN pip install https://github.com/databio/refgenconf/archive/dev.zip
 RUN pip install https://github.com/databio/refgenieserver/archive/master.zip
 
