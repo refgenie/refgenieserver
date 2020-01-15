ef1666a4bf7611b3c151873e25f28968c0800819
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-01-15 13:08:46 -0500

    check the selected config instead of RGC for input

--- refgenieserver/main.py
+++ refgenieserver/main.py
@@ -28,7 +28,7 @@ def main():
     selected_cfg = select_genome_config(args.config)
     # this RefGenConf object will be used in the server, so it's read-only
     rgc = RefGenConf(filepath=selected_cfg, writable=False)
-    assert len(rgc) > 0, "You must provide a config file or set the {} environment variable".\
+    assert selected_cfg is not None, "You must provide a config file or set the {} environment variable".\
         format("or ".join(CFG_ENV_VARS))
     if args.command == "archive":
         arp = [parse_registry_path(x) for x in args.asset_registry_paths] \
